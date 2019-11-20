fa72f74aa985cacf708ff8aff9ffc93004d133a7
OliverslottVidendjurs <oliv169n@videndjurs.net>
2019-11-20 21:07:48 +0100

    add all the classes!

--- main.js
+++ main.js
@@ -1,132 +1,166 @@
 "use strict";
-let snegle = [{
-        id: "a",
-        navn: "Dennis",
-        foto: "img/snegl1.png",
-        startY: -40,
-        x: 0,
-        y: 0
-    },
-    {
-        id: "b",
-        navn: "George",
-        foto: "img/snegl2.png",
-        startY: 40,
-        x: 0,
-        y: 0
-    },
-    {
-        id: "c",
-        navn: "Bob",
-        foto: "img/snegl3.png",
-        startY: 120,
-        x: 0,
-        y: 0
-    },
-    {
-        id: "d",
-        navn: "Harold",
-        foto: "img/snegl4.png",
-        startY: 200,
-        x: 0,
-        y: 0
-    }];
-let sek = 0;
-const minSpring = 3;
-const maxSpring = 15;
-const tidsinterval = 100;
-const finishLine = 730;
-const startX = -160;
-let winners = [];
-let racetrack = document.querySelector("#raceway");
-for (let i = 0; i < snegle.length; i++) {
-    snegle[i].x = startX;
-    snegle[i].y = snegle[i].startY;
-    let s = document.createElement("div");
-    s.id = snegle[i].id;
-    s.className = "snegle-container";
-    s.style.backgroundImage = `url("${snegle[i].foto}")`;
-    s.style.top = snegle[i].y + "px";
-    s.style.left = snegle[i].x + "px";
-    let nameContainer = document.createElement("div");
-    nameContainer.innerHTML = `<p>${snegle[i].navn}</p>`;
-    nameContainer.className = "name-container";
-    s.appendChild(nameContainer);
-    racetrack.appendChild(s);
-}
-function start() {
-    document.querySelector("#startknap").style.display = "none";
-    afsted();
-}
-function reset() {
-    for (let i = 0; i < snegle.length; i++) {
-        snegle[i].x = startX;
-        snegle[i].y = snegle[i].startY;
+class Snegl {
+    constructor(id, navn, foto) {
+        this.minSpring = 3;
+        this.maxSpring = 15;
+        this.id = id;
+        this.navn = navn;
+        this.foto = foto;
+        this.startX = -160;
+        this.x = this.startX;
+        this.y = 0;
+        this.element = null;
     }
-    winners = [];
-    sek = 0;
-    document.querySelector("#resetknap").style.display = "none";
-    document.querySelector(".resultat p").innerText = "";
-    start();
-}
-document.querySelector("#resetknap").addEventListener("click", function () {
-    reset();
-});
-function afsted() {
-    for (let i = 0; i < snegle.length; i++) {
-        snegle[i].x += spring();
-        document.querySelector(`#${snegle[i].id}`).style.left = snegle[i].x + "px";
-        if (snegle[i].x >= finishLine) {
-            winners.push(snegle[i].navn);
-        }
+    init(startY) {
+        this.y = startY;
+        this.createAndAppendElement();
     }
-    if (winners.length) {
-        winner(winners);
+    move() {
+        this.x += this.spring();
+        this.render();
+        return this;
     }
-    else {
-        setTimeout("afsted();", tidsinterval);
-        sek += 1;
+    reset() {
+        this.x = this.startX;
+        this.render();
+        return this;
     }
-}
-function winner(vinderen) {
-    let text = document.querySelector(".resultat p");
-    let tid = (sek * tidsinterval) / 1000;
-    if (vinderen.length) {
-        if (vinderen.length === 1) {
-            text.innerText = `Ræset blev vundet af ${vinderen}! Det tog ${tid} sekunder`;
+    createAndAppendElement() {
+        let s = document.createElement("div");
+        s.id = this.id;
+        s.className = "snegle-container";
+        s.style.backgroundImage = `url("${this.foto}")`;
+        s.style.top = this.y + "px";
+        s.style.left = this.x + "px";
+        this.element = s;
+        let nameContainer = document.createElement("div");
+        nameContainer.innerHTML = `<p>${this.navn}</p>`;
+        nameContainer.className = "name-container";
+        s.appendChild(nameContainer);
+        game.racetrack.appendChild(s);
+    }
+    render() {
+        if (this.element !== null) {
+            this.element.style.left = this.x + "px";
         }
         else {
-            text.innerText = `Ræset er slut - det blev udafgjort mellem: ${vinderen}! Det tog ${tid} sekunder`;
+            console.error("Call init before calling render!");
         }
     }
-    document.querySelector("#resetknap").style.display = "block";
-    let parsed = JSON.parse(localStorage.getItem("winner"));
-    if (parsed === null) {
-        parsed = [];
-    }
-    for (let vinder of vinderen) {
-        parsed.push({ name: vinder, time: tid });
-    }
-    localStorage.setItem("winner", JSON.stringify(parsed));
-    renderTop10();
-}
-function getTop10() {
-    let parsed = JSON.parse(localStorage.getItem("winner"));
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-    let top10 = parsed.sort(function (a, b) {
-        return a.time - b.time;
-    }).slice(0, 10);
-    return top10;
-}
-renderTop10();
-function renderTop10() {
-    const top10elm = document.querySelector(".top10");
-    top10elm.innerHTML = "";
-    for (let snegl of getTop10()) {
-        top10elm.innerHTML += `<li>${snegl.name}: ${snegl.time}</li>`;
+    spring() {
+        let randomStep = Math.round(Math.random() * this.maxSpring) + this.minSpring;
+        return randomStep;
     }
 }
-function spring() {
-    let randomStep = Math.round(Math.random() * maxSpring) + minSpring;
-    return randomStep;
+class Game {
+    constructor() {
+        this.winners = [];
+        this.sek = 0;
+        this.score = new Score();
+        this.racetrack = document.querySelector("#raceway");
+        this.tidsinterval = 100;
+        this.finishLine = 730;
+        this.yDifference = 100;
+        this.snegle = [
+            new Snegl("a", "Dennis", "img/snegl1.png"),
+            new Snegl("b", "George", "img/snegl2.png"),
+            new Snegl("c", "Bob", "img/snegl3.png"),
+            new Snegl("d", "Harold", "img/snegl4.png"),
+        ];
+    }
+    init() {
+        this.snegle.forEach((snegl, index) => {
+            snegl.init(-40 + this.yDifference * index);
+        });
+        this.bindEvents();
+    }
+    start() {
+        document.querySelector("#startknap").style.display = "none";
+        this.afsted();
+    }
+    reset() {
+        for (let snegl of this.snegle) {
+            snegl.reset();
+        }
+        this.winners = [];
+        this.sek = 0;
+        document.querySelector("#resetknap").style.display = "none";
+        document.querySelector(".resultat p").innerText = "";
+        game.start();
+    }
+    bindEvents() {
+        document.querySelector("#startknap").addEventListener("click", function () {
+            game.start();
+        });
+        document.querySelector("#resetknap").addEventListener("click", function () {
+            game.reset();
+        });
+    }
+    afsted() {
+        for (let snegl of this.snegle) {
+            snegl.move();
+            if (snegl.x >= this.finishLine) {
+                this.winners.push(snegl.navn);
+            }
+        }
+        if (this.winners.length) {
+            this.winner(this.winners);
+        }
+        else {
+            setTimeout(() => this.afsted(), this.tidsinterval);
+            this.sek += 1;
+        }
+    }
+    winner(vinderen) {
+        let text = document.querySelector(".resultat p");
+        let tid = (this.sek * this.tidsinterval) / 1000;
+        if (vinderen.length) {
+            if (vinderen.length === 1) {
+                text.innerText = `Ræset blev vundet af ${vinderen}! Det tog ${tid} sekunder`;
+            }
+            else {
+                text.innerText = `Ræset er slut - det blev udafgjort mellem: ${vinderen}! Det tog ${tid} sekunder`;
+            }
+        }
+        this.score.addWinnersToScoreboard(vinderen, tid);
+    }
 }
+class Score {
+    constructor() {
+        this.renderTop10();
+    }
+    addWinnersToScoreboard(winners, tid) {
+        document.querySelector("#resetknap").style.display = "block";
+        let parsed = JSON.parse(localStorage.getItem("winner"));
+        if (parsed === null) {
+            parsed = [];
+        }
+        for (let vinder of winners) {
+            parsed.push({ name: vinder, time: tid });
+        }
+        localStorage.setItem("winner", JSON.stringify(parsed));
+        this.renderTop10();
+    }
+    renderTop10() {
+        const top10elm = document.querySelector(".top10");
+        top10elm.innerHTML = "";
+        for (let snegl of this.getTop10()) {
+            top10elm.innerHTML += `<li>${snegl.name}: ${snegl.time}</li>`;
+        }
+    }
+    getTop10() {
+        let parsed = JSON.parse(localStorage.getItem("winner"));
+        if (parsed) {
+            //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+            let top10 = parsed.sort(function (a, b) {
+                return a.time - b.time;
+            }).slice(0, 10);
+            return top10;
+        }
+        else {
+            return [];
+        }
+    }
+}
+let game = new Game();
+game.init();
--- main.ts
+++ main.ts
@@ -1,159 +1,198 @@
-type snegl = {
-    id: string,
-    navn: string,
-    foto: string,
-    startY: number,
-    x: number,
-    y: number
-}
+class Snegl {
+    readonly id: string;
+    readonly navn: string;
+    readonly foto: string;
+    readonly minSpring = 3;
+    readonly maxSpring = 15;
+    x: number;
+    y: number;
+    startX: number;
+    element: HTMLDivElement | null;
 
-let snegle: snegl[] = [{
-    id: "a",
-    navn: "Dennis",
-    foto: "img/snegl1.png",
-    startY: -40,
-    x: 0,
-    y: 0
-},
-{
-    id: "b",
-    navn: "George",
-    foto: "img/snegl2.png",
-    startY: 40,
-    x: 0,
-    y: 0
-},
-{
-    id: "c",
-    navn: "Bob",
-    foto: "img/snegl3.png",
-    startY: 120,
-    x: 0,
-    y: 0
-},
-{
-    id: "d",
-    navn: "Harold",
-    foto: "img/snegl4.png",
-    startY: 200,
-    x: 0,
-    y: 0
-}];
+    constructor(id: string, navn: string, foto: string) {
+        this.id = id;
+        this.navn = navn;
+        this.foto = foto;
 
-let sek = 0;
-const minSpring = 3;
-const maxSpring = 15;
-const tidsinterval = 100;
-const finishLine = 730;
-const startX = -160;
-let winners: string[] = [];
-
-let racetrack = document.querySelector<HTMLDivElement>("#raceway");
-
-for (let i = 0; i < snegle.length; i++) {
-    snegle[i].x = startX;
-    snegle[i].y = snegle[i].startY;
-
-    let s = document.createElement("div");
-    s.id = snegle[i].id;
-    s.className = "snegle-container";
-    s.style.backgroundImage = `url("${snegle[i].foto}")`;
-    s.style.top = snegle[i].y + "px";
-    s.style.left = snegle[i].x + "px";
-    let nameContainer = document.createElement("div");
-    nameContainer.innerHTML = `<p>${snegle[i].navn}</p>`;
-    nameContainer.className = "name-container"
-    s.appendChild(nameContainer);
-    racetrack!.appendChild(s);
-}
-
-function start() {
-    document.querySelector<HTMLDivElement>("#startknap")!.style.display = "none";
-    afsted();
-}
-
-function reset() {
-    for (let i = 0; i < snegle.length; i++) {
-        snegle[i].x = startX;
-        snegle[i].y = snegle[i].startY;
+        this.startX = -160;
+        this.x = this.startX;
+        this.y = 0;
+        this.element = null;
     }
-    winners = [];
-    sek = 0;
-    document.querySelector<HTMLDivElement>("#resetknap")!.style.display = "none";
-    document.querySelector<HTMLParagraphElement>(".resultat p")!.innerText = "";
-    start();
-}
 
-document.querySelector("#resetknap")!.addEventListener("click", function () {
-    reset();
-});
+    init(startY: number) {
+        this.y = startY;
+        this.createAndAppendElement();
+    }
 
-function afsted() {
-    for (let i = 0; i < snegle.length; i++) {
-        snegle[i].x += spring();
-        document.querySelector<HTMLDivElement>(`#${snegle[i].id}`)!.style.left = snegle[i].x + "px";
-        if (snegle[i].x >= finishLine) {
-            winners.push(snegle[i].navn);
+    move() {
+        this.x += this.spring();
+        this.render();
+        return this;
+    }
+
+    reset() {
+        this.x = this.startX;
+        this.render();
+        return this;
+    }
+
+    private createAndAppendElement(){
+        let s = document.createElement("div");
+        s.id = this.id;
+        s.className = "snegle-container";
+        s.style.backgroundImage = `url("${this.foto}")`;
+        s.style.top = this.y + "px";
+        s.style.left = this.x + "px";
+        this.element = s;
+        let nameContainer = document.createElement("div");
+        nameContainer.innerHTML = `<p>${this.navn}</p>`;
+        nameContainer.className = "name-container"
+        s.appendChild(nameContainer);
+        game.racetrack!.appendChild(s);
+    }
+
+    private render(): void {
+        if (this.element !== null) {
+            this.element.style.left = this.x + "px";
+        } else {
+            console.error("Call init before calling render!");
         }
     }
-    if (winners.length) {
-        winner(winners);
-    } else {
-        setTimeout("afsted();", tidsinterval);
-        sek += 1;
-    }
 
+    private spring(): number {
+        let randomStep = Math.round(Math.random() * this.maxSpring) + this.minSpring;
+        return randomStep;
+    }
 }
 
-interface localObject {
+class Game {
+    winners: string[] = [];
+    sek = 0;
+    snegle: Snegl[];
+    score = new Score();
+    readonly racetrack = document.querySelector<HTMLDivElement>("#raceway");
+    readonly tidsinterval = 100;
+    readonly finishLine = 730;
+    readonly yDifference = 100;
+
+    constructor() {
+        this.snegle = [
+            new Snegl("a", "Dennis", "img/snegl1.png"),
+            new Snegl("b", "George", "img/snegl2.png"),
+            new Snegl("c", "Bob", "img/snegl3.png"),
+            new Snegl("d", "Harold", "img/snegl4.png"),
+        ];
+    }
+
+    init() {
+        this.snegle.forEach((snegl, index) => {
+            snegl.init(-40 + this.yDifference * index);
+        });
+        this.bindEvents();
+    }
+
+    start() {
+        document.querySelector<HTMLDivElement>("#startknap")!.style.display = "none";
+        this.afsted();
+    }
+
+    reset() {
+        for (let snegl of this.snegle) {
+            snegl.reset();
+        }
+        this.winners = [];
+        this.sek = 0;
+        document.querySelector<HTMLDivElement>("#resetknap")!.style.display = "none";
+        document.querySelector<HTMLParagraphElement>(".resultat p")!.innerText = "";
+        game.start();
+    }
+
+    private bindEvents() {
+        document.querySelector<HTMLDivElement>("#startknap")!.addEventListener("click", function () {
+            game.start();
+        });
+
+        document.querySelector<HTMLDivElement>("#resetknap")!.addEventListener("click", function () {
+            game.reset();
+        });
+    }
+
+    private afsted() {
+        for(let snegl of this.snegle){
+            snegl.move();
+            if (snegl.x >= this.finishLine) {
+                this.winners.push(snegl.navn);
+            }
+        }
+        if (this.winners.length) {
+            this.winner(this.winners);
+        } else {
+            setTimeout(() => this.afsted(), this.tidsinterval);
+            this.sek += 1;
+        }
+    }
+
+    private winner(vinderen: string[]) {
+        let text = document.querySelector<HTMLParagraphElement>(".resultat p");
+        let tid = (this.sek * this.tidsinterval) / 1000;
+        if (vinderen.length) {
+            if (vinderen.length === 1) {
+                text!.innerText = `Ræset blev vundet af ${vinderen}! Det tog ${tid} sekunder`
+            } else {
+                text!.innerText = `Ræset er slut - det blev udafgjort mellem: ${vinderen}! Det tog ${tid} sekunder`;
+            }
+        }
+        this.score.addWinnersToScoreboard(vinderen, tid);
+    }
+}
+
+
+interface winnerStorageType {
     name: string,
     time: number
 }
 
-function winner(vinderen: string[]) {
-    let text = document.querySelector<HTMLParagraphElement>(".resultat p");
-    let tid = (sek * tidsinterval) / 1000;
-    if (vinderen.length) {
-        if (vinderen.length === 1) {
-            text!.innerText = `Ræset blev vundet af ${vinderen}! Det tog ${tid} sekunder`
-        } else {
-            text!.innerText = `Ræset er slut - det blev udafgjort mellem: ${vinderen}! Det tog ${tid} sekunder`;
+class Score {
+    constructor() {
+        this.renderTop10();
+    }
+
+    addWinnersToScoreboard(winners: string[], tid: number){
+        document.querySelector<HTMLDivElement>("#resetknap")!.style.display = "block";
+        let parsed: winnerStorageType[] = JSON.parse(localStorage.getItem("winner") as string);
+        if (parsed === null) {
+            parsed = [];
+        }
+        for (let vinder of winners) {
+            parsed.push({ name: vinder, time: tid });
+        }
+        localStorage.setItem("winner", JSON.stringify(parsed));
+        this.renderTop10();
+    }
+
+    private renderTop10() {
+        const top10elm = document.querySelector<HTMLUListElement>(".top10");
+        top10elm!.innerHTML = "";
+
+        for (let snegl of this.getTop10()) {
+            top10elm!.innerHTML += `<li>${snegl.name}: ${snegl.time}</li>`
         }
     }
-    document.querySelector<HTMLDivElement>("#resetknap")!.style.display = "block";
 
-    let parsed: localObject[] = JSON.parse(localStorage.getItem("winner") as string);
-    if(parsed === null){
-        parsed = [];
-    }
-    for (let vinder of vinderen) {
-        parsed.push({ name: vinder, time: tid });
-    }
-    localStorage.setItem("winner", JSON.stringify(parsed));
-    renderTop10();
-    
-}
-
-function getTop10(): localObject[]{
-    let parsed: localObject[] = JSON.parse(localStorage.getItem("winner") as string);
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-    let top10 = parsed.sort(function(a,b){
-        return a.time - b.time;
-    }).slice(0, 10);   
-    return top10; 
-}
-
-renderTop10();
-function renderTop10(){
-    const top10elm = document.querySelector<HTMLUListElement>(".top10");
-    top10elm!.innerHTML = "";
-
-    for(let snegl of getTop10()){
-        top10elm!.innerHTML += `<li>${snegl.name}: ${snegl.time}</li>`
+    private getTop10(): winnerStorageType[] {
+        let parsed: winnerStorageType[] = JSON.parse(localStorage.getItem("winner") as string);
+        if (parsed) {
+            //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+            let top10 = parsed.sort(function (a, b) {
+                return a.time - b.time;
+            }).slice(0, 10);
+            return top10;
+        } else {
+            return [];
+        }
     }
 }
 
-function spring(): number {
-    let randomStep = Math.round(Math.random() * maxSpring) + minSpring;
-    return randomStep;
-}
+let game = new Game();
+game.init();
